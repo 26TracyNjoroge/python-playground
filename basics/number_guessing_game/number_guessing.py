9173c18f79dc12e1305729bf0852ad1cf9b5abb2
Tracy Njoroge <tracywangari997@gmail.com>
2025-09-29 23:25:24 +0300

    Update the game

--- basics/number_guessing_game/number_guessing.py
+++ basics/number_guessing_game/number_guessing.py
@@ -1,9 +1,10 @@
-"""Number Gueassing Game"""
+"""Number Guessing Game"""
 
 import random
 
 secret_number = random.randint(1, 15)
 attempts = 0
+max_attempts = 5
 
 print("This is a number guessing game. You have to guess a number between 1 and 15!")
 
@@ -12,14 +13,17 @@ while True:
     attempts += 1
 
     if  guess == secret_number:
-        print("You have guessed correctly!")
+        print(f"You have guessed correctly in {attempts} attempts!")
         break
     elif guess < secret_number:
         print("Too low!")
     elif guess > secret_number:
         print("Too high!")
 
-print(f"{attempts}")
+    if attempts == max_attempts and guess != secret_number:
+        print(f"Game over! The correct number is {secret_number}.")
+
+
 
 
 
